fbc36948df939a90382cb3821002e85b8e5aee55
ISenseAura <mayurhiwale2451982@gmail.com>
2025-12-07 23:10:57 +0530

    Config: add status, lockedRooms

--- src/config/bot.example.ts
+++ src/config/bot.example.ts
@@ -1,20 +1,21 @@
-
 abstract class BotConfig {
-    abstract prefixes: string[];
-    abstract developers: string[];
-    abstract rooms: string[];
-    abstract username: string;
-    abstract password: string;
+  abstract prefixes: string[];
+  abstract developers: string[];
+  abstract rooms: string[];
+  abstract username: string;
+  abstract password: string;
 }
 
 class Config extends BotConfig {
-    prefixes = ["."];
-    developers = ['p9'];
-    rooms = ['botdevelopment']
-    username = "my3amthoughts";
-    password = "SIX_SEVEN";
+  username = "my3amthoughts";
+  password = "SIX_SEVEN";
+  prefixes = ["."];
+  developers = ["p9"];
+  rooms = ["botdevelopment"];
+  lockedRooms = [""];
 
-    commandsDir = 'src/commands';
+  status = "I'm as real as you are"
+  commandsDir = "src/commands";
 }
 
 export default new Config();
