0016d1e17e727b87557834e35a5617ba41cf7c01
ISenseAura <mayurhiwale2451982@gmail.com>
2025-12-07 13:24:21 +0530

    Add permissions module

---
+++ src/permissions.ts
@@ -0,0 +1,5 @@
+export type Perms =  "" | "+" | "%" | "@" | "*" | "#" | "&" | "~" | "dev";
+export const ranks: Perms[] = ['', '+', '%', '@', '*', '#', '&', '~', 'dev']
+export const psRanks: Perms[] = ['', '+', '%', '@', '*', '#', '&', '~']
+
+//TODO MODULE
