f000a6d0c0acdac825dfe8490a5ac2296396e10c
ISenseAura <mayurhiwale2451982@gmail.com>
2025-12-07 23:28:59 +0530

    make discord, ps and storage modules global

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,9 @@ declare global {
     var Commands: Commands;
     var Config: import("./config/bot").default;
     var toId: (text: string) => string;
+    var PS: import('./ps').default;
+    var Discord: import('./discord').default;
+    var Db: import('./lib/storage').default
 
     var Users: import("./user").Users;
     var Rooms: import("./room").Rooms;
